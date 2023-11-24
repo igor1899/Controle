45411c57f25feea6c34c4b32a5531d3e3a02e07e
igor1899 <80854665+igor1899@users.noreply.github.com>
2023-11-23 21:50:09 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,1 @@
-<?php
-    define('HOST', 'monorail.proxy.rlwy.net');
-    define('USER', 'root');
-    define('PASS', '4-2CCD3g-DE4hH-aC5cc--EddEEE3fEc');
-    define('BASE', 'railway');
-    
-    $mysqli = new MySQLi(HOST,USER,PASS,BASE);
-    
-// Verificar a conexão
-if ($mysqli->connect_error) {
-    die("Conexão falhou: " . $mysqli->connect_error);
-}
-?>
+
