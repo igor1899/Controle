6c2718e1fd093eab1fe511a939d302df5e0c3c0c
igor1899 <80854665+igor1899@users.noreply.github.com>
2023-11-24 17:06:03 -0300

    Update protect.php

--- protect.php
+++ protect.php
@@ -4,9 +4,7 @@ if ( !isset($_SESSION)) {
   session_start();
 }
 
-if ( !isset($_SESSION['CPF'])) {
-    die("Você não está logado para acessar essa página.<p><a href=\"login.php\">Entrar</a></p>");
-}
-
+if ( !isset($_SESSION['CPF'])) 
 
 ?>
+<meta http-equiv="refresh" content="0; url=login.php";
