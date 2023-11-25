6f5421cde7380918be4c0559d0d21d5c61db808b
igor1899 <80854665+igor1899@users.noreply.github.com>
2023-11-24 21:45:50 -0300

    Update protect.php

--- protect.php
+++ protect.php
@@ -5,6 +5,6 @@ if ( !isset($_SESSION)) {
 }
 
 if ( !isset($_SESSION['CPF'])) {
-    die("Você não está logado para acessar essa página.<p><a href=\"login.php\">Entrar</a></p>");
-}
+  header("location: login.php");
+  }
 ?>
