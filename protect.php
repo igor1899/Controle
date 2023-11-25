6ccb42554deb082d9904071b82357320c8ee8c0a
igor1899 <80854665+igor1899@users.noreply.github.com>
2023-11-24 21:56:12 -0300

    Update protect.php

--- protect.php
+++ protect.php
@@ -1,11 +1,12 @@
 <?php
 
-if ( !isset($_SESSION)) {
+if (!isset($_SESSION)) {
   session_start();
 }
 
-if ( !isset($_SESSION['CPF'])) 
-else{
+if (!isset($_SESSION['CPF'])) {
   header("location: login.php");
-  }
+} else {
+  session_start();
+}
 ?>
