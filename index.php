5e22914027f89a545272450d76236b6174d3153f
igor1899 <80854665+igor1899@users.noreply.github.com>
2023-11-25 18:56:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,63 @@
 <?php
+//Conexão com o Banco de Dados
+include('config.php'); 
 
-include('protect.php');
+if(isset($_POST['CPF'] ) || isset($_POST['Senha'])) {
 
+        if(strlen($_POST['CPF']) == 0 ) {
+          echo "<script>alert('Preencha com o seu CPF') </script>";
+        } else if(strlen($_POST['Senha']) == 0 ) {
+          echo "<script>alert('Preencha com a sua senha') </script>";
+        } else {
+          //Coleta as informações inseridas e armazena em CPF e Senha:
+            $CPF = $mysqli->real_escape_string($_POST['CPF']);
+            $Senha = $mysqli->real_escape_string($_POST['Senha']);
+
+          //SQL que verifica se existe o usuário para fazer Login no sistema:
+            $sql_code = "SELECT * FROM colaboradores WHERE cpf_colab = '$CPF' AND senha_colab = '$Senha'" ;
+            $sql_query = $mysqli->query($sql_code) or die ("Falha na execução:". $mysqli->error);
+            $quant = $sql_query->num_rows;
+
+            if ($quant == 1) {
+                $colaboradores = $sql_query->fetch_assoc();
+                if(!isset($_SESSION)) {
+                    session_start();
+                }
+                $_SESSION['CPF'] = $colaboradores['cpf_colab'];
+                $_SESSION['Nome'] = $colaboradores['nome_colab'];
+
+                header("location: index.php");
+
+            } else {
+              echo "<script>alert('Falha ao logar! Email e/ou senha incorretos') </script>";
+            }
+        }
+}
 ?>
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html> 
+<html lang="pt-BR">
   <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <meta charset="utf-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" type="text/css" href="style.css">
+     <title>Tela de Login</title>
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
-    <title>Cadastro</title>
   </head>
   <body>
+    <form class="login" action="" method="POST" >
+        <h2>Login</h2>
+        <div class="box-user">
+        <input type="text" name="CPF">
+          <label>CPF</label>
+        </div>
+        <div class="box-user">
+        <input type="password" name="Senha">
+          <label>Senha</label>
+        </div>
+        <div>
+        <button type="submit">Entrar</button>
+    </form>
 
-    Seja bem vindo, <?php echo $_SESSION['Nome'];?>
-
-    <p>
-    <a href="sair.php">Sair</a>
-    </P>
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-
-  <div class="conteiner-fluid">
-        <a class="navbar-brand" href="index.php">Inicio</a>
-        <a class="navbar-brand" href="?page=novo">Novo Usuário</a>
-        <a class="navbar-brand" href="?page=listar">Listar usuários</a>
-
-  <div class="container">
-    <div class="row">
-      <div class="col mt-5">
-          <?php
-            include("config.php");
-            switch(@$_REQUEST["page"]) {
-              case "novo": include("novo-usuario.php");
-              break;
-              case "listar": include("listar-usuario.php");
-              break;
-              case "editar": include("editar-usuario.php");
-              break;
-              case "salvar": include("salvar-usuario.php");
-              break;
-              default:
-              print "<h1>Bem vindo!</h1>";
-            }
-          ?>
-      </div>
-    </div>
-  </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
 </html>
