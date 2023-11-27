6edd1d2135b9fbaf9bda32b138f3b5d2902f9d87
igor1899 <80854665+igor1899@users.noreply.github.com>
2023-11-27 20:55:09 -0300

    Update novo-usuario.php

--- novo-usuario.php
+++ novo-usuario.php
@@ -1,10 +1,24 @@
 <h1> Novo Usuário </h1>
 <form action="?page=salvar" method="POST">
     <input type="hidden" name="acao" value="cadastrar">
+    
     <div class="mb-3">
-       <label>CPF do colaborador (somente os números)</label>
-       <input type="text" name="cpf_colab" maxlength="11" class="form-control">
-    </div>
+    <label>CPF do colaborador (somente os números)</label>
+    <input type="text" name="cpf_colab" maxlength="11" class="form-control" oninput="validateCPFLength(this)">
+</div>
+
+<script>
+    function validateCPFLength(inputField) {
+        var maxLength = parseInt(inputField.getAttribute('maxlength'));
+        var currentValue = inputField.value;
+
+        if (currentValue.length < maxLength) {
+            // O CPF tem menos de 11 caracteres
+            // Aqui você pode adicionar lógica adicional ou uma mensagem de erro, se necessário
+            console.log('CPF incompleto. Insira os 11 dígitos.');
+        }
+    }
+</script>
 
     <div class="mb-3">
        <label>Nome</label>
